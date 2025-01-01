9c3a10f12ed3760e7dd2f23fbf9b01c96c7545ce
Aayush Pandit <aayushs.pandit01@gmail.com>
2025-01-01 21:46:37 +0100

    cross origin error resolve

--- src/main/java/com/zosh/controllers/TodoController.java
+++ src/main/java/com/zosh/controllers/TodoController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/todos")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://azuretodoappbe-b6h7ftgbgufkh7bj.canadacentral-01.azurewebsites.net")
 public class TodoController {
     private TodoService todoService;
 
